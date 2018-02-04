edd1fc78b7d5f2f614279be39f7124619f4e820c
Ujjwal Verma <ujjwalverma1111@gmail.com>
2018-02-05 02:54:11 +0530

    Working encryption/decryption

---
+++ decrypt.py
@@ -0,0 +1,32 @@
+import os
+
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+
+def rsa_decrypt(data: bytes, private_key: RSA._RSAobj):
+    cipher = PKCS1_OAEP.new(private_key)
+    message = cipher.decrypt(data)
+    return message
+
+
+def aes_decrypt(key: bytes, data: bytes):
+    iv = data[:AES.block_size]
+    message = data[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    return cipher.decrypt(message)
+
+
+def decrypt(private_key, file_path):
+    private_key = RSA.importKey(private_key)
+    key_size = (private_key.size() + 1) // 8
+
+    with open(file_path, "rb") as f:
+        enc_key = f.read(key_size)
+        enc_data = f.read()
+
+    key = rsa_decrypt(enc_key, private_key)
+    data = aes_decrypt(key, enc_data)
+
+    with open(os.path.splitext(file_path)[0], "wb") as f:
+        f.write(data)
